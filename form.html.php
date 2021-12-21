97f2a2cfd9cfa748790120663f0b859feef23106
Mariusz Bielicki <mbielicki@wp.pl>
2021-12-21 17:18:44 +0100

    Adding a category

--- form.html.php
+++ form.html.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 
-<form action="?" method="post">
+<form action="?" method="post" name="adding_quote">
 
 	<p><label for="quotetext">Enter a quote</label></p>
 
@@ -38,6 +38,25 @@
 	<p><input type="submit" value="Add"><p>
 
 </form>
+
+<label for="category">Enter first category</label>
+
+<select id="category" name="category_list" form="adding_quote" multiple size=2>
+
+	<option value="politician">Politician</option>
+	<option value="scientist">Scientist</option>
+	<option value="writer">Writer</option>
+	<option value="activist">Activist</option>
+	<option value="philosopher">Philosopher</option>
+	<option value="poet">Poet</option>
+	<option value="coach">Coach</option>
+	<option value="american">American</option>
+	<option value="british">British</option>
+	<option value="african">African</option>
+	<option value="producer">Producer</option>
+	<option value="businessman">Businessman</option>
+
+</select>
 
 </body>
 </html>
